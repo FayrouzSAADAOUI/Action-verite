a7b64479a1d3e379313af2ea45c3cbde47e1728f
FayrouzSAADAOUI <fayrouz.saadaoui@ynov.com>
2025-10-27 11:33:57 +0100

    Initial commit - projet Ionic Action ou Vérité

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
+  pseudo: string = '';
+  genre: string = '';
+  players: { pseudo: string; genre: string }[] = [];
+
+  constructor(private router: Router) {
+    // Charger les joueurs depuis le localStorage
+    const storedPlayers = localStorage.getItem('players');
+    if (storedPlayers) this.players = JSON.parse(storedPlayers);
+  }
+
+  addPlayer() {
+    if (this.pseudo && this.genre) {
+      this.players.push({ pseudo: this.pseudo, genre: this.genre });
+      this.pseudo = '';
+      this.genre = '';
+      localStorage.setItem('players', JSON.stringify(this.players));
+    }
+  }
+
+  removePlayer(index: number) {
+    this.players.splice(index, 1);
+    localStorage.setItem('players', JSON.stringify(this.players));
+  }
+
+  startGame() {
+    this.router.navigate(['/mode']); // la page Mode sera créée ensuite
+  }
 }
